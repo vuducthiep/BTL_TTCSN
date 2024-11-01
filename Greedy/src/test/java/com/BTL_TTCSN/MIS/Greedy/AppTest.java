1342b48cd91ede6902a4e6f7fd7e464def1d635d
Vũ Đức Thiệp <vuducthiep011104@gmail.com>
2024-11-01 22:31:59 +0700

    update Greedy

---
+++ Greedy/src/test/java/com/BTL_TTCSN/MIS/Greedy/AppTest.java
@@ -0,0 +1,19 @@
+package com.BTL_TTCSN.MIS.Greedy;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
